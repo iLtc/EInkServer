81ae943ceb4dd485d52c55a1de6353581327f370
Alan Luo <i@iltc.io>
2020-06-02 10:11:11 -0400

    Added Weather and Habitica APIs

--- eink/__init__.py
+++ eink/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -11,12 +11,8 @@ def create_app(test_config=None):
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py')
 
     # ensure the instance folder exists
     try:
@@ -24,9 +20,7 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    from . import api
+    app.register_blueprint(api.bp)
 
     return app
--- eink/api.py
+++ eink/api.py
@@ -1,4 +1,68 @@
-from flask import Blueprint
+from flask import Blueprint, current_app, request
+import requests
 
-bp = Blueprint('api', __name__)
+bp = Blueprint('api', __name__, url_prefix='/api')
 
+
+@bp.before_request
+def before_request():
+    if request.args.get('token', '') != current_app.config['EINK_TOKEN']:
+        return {
+            'status': 'fail',
+            'reason': 'Miss EINK Token'
+        }
+
+
+@bp.route('/weather')
+def weather():
+    r = requests.get(
+        'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units=imperial&appid={}'.format(
+            current_app.config['WEATHER_LAT'],
+            current_app.config['WEATHER_LON'],
+            current_app.config['OPENWEATHERMAP_APPID']
+        ))
+
+    if r.status_code == requests.codes.ok:
+        data = r.json()
+
+        results = {
+            'status': 'success',
+            'current': data['current'],
+            'today': data['daily'][0],
+            'tomorrow': data['daily'][1]
+        }
+
+    else:
+        results = {
+            'status': 'fail',
+            'reason': 'HTTP Request Returns ' + str(r.status_code)
+        }
+
+    return results
+
+
+@bp.route('/habitica')
+def habitica():
+    r = requests.get(
+        'https://habitica.com/api/v3/tasks/user',
+        headers={
+            'x-api-user': current_app.config['HABITICA_USERID'],
+            'x-api-key': current_app.config['HABITICA_TOKEN']
+        }
+    )
+
+    if r.status_code == requests.codes.ok:
+        data = r.json()
+
+        results = {
+            'status': 'success',
+            'data': data['data']
+        }
+
+    else:
+        results = {
+            'status': 'fail',
+            'reason': 'HTTP Request Returns ' + str(r.status_code)
+        }
+
+    return results
