ff7d039e698b4238986c8d7c3595c91f6db43d22
Alan Luo <i@iltc.io>
2020-06-14 23:45:07 -0400

    Fix some bugs

--- client.py
+++ client.py
@@ -3,27 +3,31 @@ import config
 from PIL import Image
 from io import BytesIO
 import sys
+import random
 
 
 def server(debug=False):
-    r = requests.get('{}/refresh?token={}'.format(
+    r = requests.get('{}/refresh?token={}&t={}'.format(
         config.SERVER_DOMAIN,
-        config.SERVER_TOKEN
+        config.SERVER_TOKEN,
+        random.random()
     ))
 
     if r.status_code == requests.codes.ok:
-        data = requests.get('{}/red.bmp?token={}'.format(
+        data = requests.get('{}/red.bmp?token={}&t={}'.format(
             config.SERVER_DOMAIN,
-            config.SERVER_TOKEN
+            config.SERVER_TOKEN,
+            random.random()
         ))
 
         image = Image.open(BytesIO(data.content))
 
         image.save('red.bmp')
 
-        data = requests.get('{}/black.bmp?token={}'.format(
+        data = requests.get('{}/black.bmp?token={}&t={}'.format(
             config.SERVER_DOMAIN,
-            config.SERVER_TOKEN
+            config.SERVER_TOKEN,
+            random.random()
         ))
 
         image = Image.open(BytesIO(data.content))
@@ -31,9 +35,10 @@ def server(debug=False):
         image.save('black.bmp')
 
         if debug:
-            data = requests.get('{}/debug.bmp?token={}'.format(
+            data = requests.get('{}/debug.bmp?token={}&t={}'.format(
                 config.SERVER_DOMAIN,
-                config.SERVER_TOKEN
+                config.SERVER_TOKEN,
+                random.random()
             ))
 
             image = Image.open(BytesIO(data.content))
@@ -68,10 +73,12 @@ if __name__ == '__main__':
         print('python3 eink.py [server [debug]] [client [clear]]')
 
     if len(sys.argv) >= 2:
+        server_result = True
         if 'server' in sys.argv:
-            red_image, black_image = server(debug=('debug' in sys.argv))
+            debug = True if 'debug' in sys.argv else False
+            server_result = server(debug)
 
-        if 'client' in sys.argv:
+        if 'client' in sys.argv and server_result:
             if 'clear' in sys.argv:
                 client(clear=True)
             else:
